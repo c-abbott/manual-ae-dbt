6417921ed5002c3f67cc4c66386859a2d4e81f59
c-abbott <cllmbbtt@gmail.com>
2025-02-03 14:21:16 +0000

    Add business_category and country dimensions

--- manual/models/marts/customer_cohorts.sql
+++ manual/models/marts/customer_cohorts.sql
@@ -1,9 +1,15 @@
 -- This query assigns each customer to a cohort based on their first subscription month
 WITH cohort AS (
     SELECT
-        customer_id,
-        MIN(DATE_TRUNC(subscription_start_date, MONTH)) AS cohort_month
-    FROM {{ ref('stg_activity') }}
-    GROUP BY customer_id
+        a.customer_id,
+        c.customer_country,
+        o.business_category,
+        MIN(DATE_TRUNC(a.subscription_start_date, MONTH)) AS cohort_month
+    FROM {{ ref('stg_activity') }} a
+    JOIN {{ ref('stg_customers') }} c
+    ON a.customer_id = c.customer_id
+    JOIN {{ ref('stg_acq_orders') }} o
+    ON a.customer_id = o.customer_id
+    GROUP BY a.customer_id, c.customer_country, o.business_category
 )
 SELECT * FROM cohort
--- manual/models/marts/customer_retention.sql
+++ manual/models/marts/customer_retention.sql
@@ -3,6 +3,8 @@ WITH cohort_data AS (
     SELECT
         a.customer_id,
         c.cohort_month,
+        c.customer_country,
+        c.business_category,
         DATE_TRUNC(a.subscription_start_date, MONTH) AS activity_month,
         a.subscription_end_date
     FROM {{ ref('stg_activity') }} a
@@ -14,39 +16,51 @@ cohort_retention AS (
     SELECT
         cohort_month,
         activity_month,
+        customer_country,
+        business_category,
         COUNT(DISTINCT customer_id) AS total_active_users
     FROM cohort_data
     WHERE activity_month >= cohort_month
-    GROUP BY cohort_month, activity_month
+    GROUP BY cohort_month, activity_month, customer_country, business_category
 ),
--- Determines the total number of users in each cohort
+-- Determines the total number of users in each cohort, segmented by country and business category
 cohort_size AS (
     SELECT
         cohort_month,
+        customer_country,
+        business_category,
         COUNT(DISTINCT customer_id) AS cohort_size
     FROM cohort_data
-    GROUP BY cohort_month
+    GROUP BY cohort_month, customer_country, business_category
 ),
 -- Identifies the initial activity month for each customer
 initial_cohort_retention AS (
     SELECT
         c.cohort_month,
+        c.customer_country,
+        c.business_category,
         a.customer_id,
         MIN(DATE_TRUNC(a.subscription_start_date, MONTH)) OVER (PARTITION BY c.customer_id) AS initial_activity_month
     FROM {{ ref('stg_activity') }} a
     JOIN {{ ref('customer_cohorts') }} c
     ON a.customer_id = c.customer_id
 )
--- Final query to compute retention rates and track cohort activity
+-- Final query to compute retention rates and track cohort activity, segmented by country and business category
 SELECT 
     cr.cohort_month,
     cr.activity_month,
+    cr.customer_country,
+    cr.business_category,
     cr.total_active_users,
     cs.cohort_size,
     ROUND(100.0 * cr.total_active_users / cs.cohort_size, 2) AS retention_rate,
     icr.initial_activity_month
 FROM cohort_retention cr
 JOIN cohort_size cs
-ON cr.cohort_month = cs.cohort_month
+ON cr.cohort_month = cs.cohort_month 
+AND cr.customer_country = cs.customer_country 
+AND cr.business_category = cs.business_category
 JOIN initial_cohort_retention icr
-ON cr.cohort_month = icr.cohort_month
+ON cr.cohort_month = icr.cohort_month 
+AND cr.customer_country = icr.customer_country 
+AND cr.business_category = icr.business_category
