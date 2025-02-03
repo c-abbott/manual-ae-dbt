c1bdd7d987795db6630bafb0c6899d5d2935b566
c-abbott <cllmbbtt@gmail.com>
2025-02-03 14:11:09 +0000

    business_category rather than acq_category

--- manual/models/staging/stg_acq_orders.sql
+++ manual/models/staging/stg_acq_orders.sql
@@ -1,5 +1,5 @@
 SELECT 
     customer_id, 
-    taxonomy_business_category_group AS acquisition_category
+    taxonomy_business_category_group AS business_category
 FROM 
     {{ source('manual', 'acq_orders') }}
