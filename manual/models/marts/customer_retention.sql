24081149d4d893ac75a4abe300dff2ed8f2269a9
c-abbott <cllmbbtt@gmail.com>
2025-02-03 14:10:53 +0000

    Initial retention model

---
+++ manual/models/marts/customer_retention.sql
@@ -0,0 +1,52 @@
+-- This query joins cohort data with activity data to track subscription activity over time
+WITH cohort_data AS (
+    SELECT
+        a.customer_id,
+        c.cohort_month,
+        DATE_TRUNC(a.subscription_start_date, MONTH) AS activity_month,
+        a.subscription_end_date
+    FROM {{ ref('stg_activity') }} a
+    JOIN {{ ref('customer_cohorts') }} c
+    ON a.customer_id = c.customer_id
+),
+-- Calculates the number of active users for each cohort and month
+cohort_retention AS (
+    SELECT
+        cohort_month,
+        activity_month,
+        COUNT(DISTINCT customer_id) AS total_active_users
+    FROM cohort_data
+    WHERE activity_month >= cohort_month
+    GROUP BY cohort_month, activity_month
+),
+-- Determines the total number of users in each cohort
+cohort_size AS (
+    SELECT
+        cohort_month,
+        COUNT(DISTINCT customer_id) AS cohort_size
+    FROM cohort_data
+    GROUP BY cohort_month
+),
+-- Identifies the initial activity month for each customer
+initial_cohort_retention AS (
+    SELECT
+        c.cohort_month,
+        a.customer_id,
+        MIN(DATE_TRUNC(a.subscription_start_date, MONTH)) OVER (PARTITION BY c.customer_id) AS initial_activity_month
+    FROM {{ ref('stg_activity') }} a
+    JOIN {{ ref('customer_cohorts') }} c
+    ON a.customer_id = c.customer_id
+)
+-- Final query to compute retention rates and track cohort activity
+SELECT 
+    cr.cohort_month,
+    cr.activity_month,
+    cr.total_active_users,
+    cs.cohort_size,
+    ROUND(100.0 * cr.total_active_users / cs.cohort_size, 2) AS retention_rate,
+    icr.initial_activity_month
+FROM cohort_retention cr
+JOIN cohort_size cs
+ON cr.cohort_month = cs.cohort_month
+JOIN initial_cohort_retention icr
+ON cr.cohort_month = icr.cohort_month
