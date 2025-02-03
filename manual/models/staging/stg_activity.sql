9787594c446e829f46658a23b891de6fd0281829
c-abbott <cllmbbtt@gmail.com>
2025-02-03 16:12:00 +0000

    Deal with duplicate subscriptions in activity table

--- manual/models/staging/stg_activity.sql
+++ manual/models/staging/stg_activity.sql
@@ -1,7 +1,21 @@
+-- Deduplicate activity date
+WITH activity_raw AS (
+    SELECT 
+        customer_id, 
+        subscription_id, 
+        CAST(from_date AS DATE) AS subscription_start_date,
+        CAST(to_date AS DATE) AS subscription_end_date,
+        ROW_NUMBER() OVER (
+            PARTITION BY customer_id, subscription_id, from_date, to_date 
+            ORDER BY from_date DESC
+        ) AS row_num
+    FROM {{ source('manual', 'activity') }}
+)
+
 SELECT 
     customer_id, 
     subscription_id, 
-    CAST(from_date AS DATE) AS subscription_start_date,
-    CAST(to_date AS DATE) AS subscription_end_date
-FROM
-    {{ source('manual', 'activity') }}
+    subscription_start_date,
+    subscription_end_date
+FROM activity_raw
+WHERE row_num = 1
